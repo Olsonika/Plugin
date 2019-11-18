b8f067f619face1f8ae3e85023ef9a8117e83af4
Olsonika <alek2257@easv365.dk>
2019-11-18 22:46:15 +0100

    StableVersion

--- nobadwords.php
+++ nobadwords.php
@@ -3,9 +3,10 @@
  * Plugin Name: No Bad Words
  * Description: No Bad Words Plugin is helping you avoid using swear words in you posts.
  * Author: Aleksandra Maryla Kurdelska
+ * Version: 0.5
  */
 
-defined( 'ABSPATH') or die ('Go away');
+defined( 'ABSPATH') or die ('Go away!');
 
  add_filter ('the_content', array('filterhook_badwords_wordpress','fix_spelling'));
  add_action( 'wp_enqueue_scripts', array('filterhook_badwords_wordpress', 'includes'));
@@ -15,24 +16,24 @@ class filterhook_badwords_wordpress{
     {
         $search  = array( 'ass','fuck', 'shit', 'motherfucker', 'cunt', 'bitch', 'asshole', 'dick', 'dickhead', 'son of a bitch', 'bastard', 'twat', 'damn', 'crap', 'nigga', 'whore', 'slut', 'prick',);
         $replace = array( 
-            "<p class='modified'>" . 'abs' . "</p>",
-            "<p class='modified'>" . 'fork' . "</p>",
-            "<p class='modified'>" . 'shirt' . "</p>",
-            "<p class='modified'>" . 'mothertrucker' . "</p>",
-            "<p class='modified'>" . 'calm' . "</p>",
-            "<p class='modified'>" . 'bench' . "</p>",
-            "<p class='modified'>" . 'armhole' . "</p>",
-            "<p class='modified'>" . 'duck' . "</p>", 
-            "<p class='modified'>" . 'duckhead' . "</p>",
-            "<p class='modified'>" . 'son of a bench' . "</p>",
-            "<p class='modified'>" . 'backflash' . "</p>",
-            "<p class='modified'>" . 'tweet' . "</p>",
-            "<p class='modified'>" . 'darling' . "</p>",
-            "<p class='modified'>" . 'carb' . "</p>",
-            "<p class='modified'>" . 'nibba' . "</p>",
-            "<p class='modified'>" . 'whole' . "</p>",
-            "<p class='modified'>" . 'flute' . "</p>",
-            "<p class='modified'>" . 'prank' . "</p>",
+            "<a class='modified'>" . 'abs' . "</a>",
+            "<a class='modified'>" . 'fork' . "</a>",
+            "<a class='modified'>" . 'shirt' . "</a>",
+            "<a class='modified'>" . 'mothertrucker' . "</a>",
+            "<a class='modified'>" . 'calm' . "</a>",
+            "<a class='modified'>" . 'bench' . "</a>",
+            "<a class='modified'>" . 'armhole' . "</a>",
+            "<a class='modified'>" . 'duck' . "</a>", 
+            "<a class='modified'>" . 'duckhead' . "</a>",
+            "<a class='modified'>" . 'son of a bench' . "</a>",
+            "<a class='modified'>" . 'pumpkin' . "</a>",
+            "<a class='modified'>" . 'tweet' . "</a>",
+            "<a class='modified'>" . 'darling' . "</a>",
+            "<a class='modified'>" . 'carb' . "</a>",
+            "<a class='modified'>" . 'nibba' . "</a>",
+            "<a class='modified'>" . 'whole' . "</a>",
+            "<a class='modified'>" . 'flute' . "</a>",
+            "<a class='modified'>" . 'prank' . "</a>",
             
         );
 
@@ -41,8 +42,6 @@ class filterhook_badwords_wordpress{
     }
 
    
-
-
     function includes ()
     {
         wp_enqueue_style ("style", plugins_url() . "/alexplug/style.css");
